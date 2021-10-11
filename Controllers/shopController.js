e1bc29307f61ec809a169615bd1f3a5af2fe113a
michael whalen <michael.whalen@sitechhs.com>
2021-10-11 11:41:56 -0400

    sample

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,7 +1,13 @@
+exports.middlewareSample = (req, res, next) => {
+  req.name = "TEST"; //this runs before the response is sent to the user
+  next(); //cues the next step of the response to occur
+};
+
 exports.homePage = (req, res) => {
   const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
   try {
-    res.json(stores);
+    console.log(req.name); //we get req.name from the middleware but WE MUST call it in index
+    res.json([stores, req.name]); //if we want to send multiple "things" back to the user we need to use an array or object. Can't simply use , as that denotes status codes
   } catch (error) {
     console.log(error);
   }
--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
-router.get("/", shopController.homePage);
+router.get("/", shopController.middlewareSample, shopController.homePage); //middlware runs, then when complete, calls next part of requst
 
 module.exports = router;

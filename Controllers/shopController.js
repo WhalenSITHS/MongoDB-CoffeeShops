f6e402315ba02a2cdf9a52e7f8fc14e02eae17a7
michael whalen <michael.whalen@sitechhs.com>
2021-10-11 12:10:56 -0400

    sdfsdf

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,24 +1,3 @@
-exports.middlewareSample = (req, res, next) => {
-  req.name = "TEST"; //this runs before the response is sent to the user
-  next(); //cues the next step of the response to occur
-};
-//for the next example we need to set up some additional middleware in app.js to allow the requests body to be readable
-exports.authMiddleware = (req, res, next) => {
-  if (req.body.user) {
-    next();
-  } else {
-    //throw new Error("No user");
-    res.json("you must be signed in");
-  }
-};
-exports.authPage = (req, res, next) => {
-  try {
-    res.json(req.body.user);
-  } catch (error) {
-    console.log(error);
-    res.json("User not signed in");
-  }
-};
 exports.homePage = (req, res) => {
   const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
   try {
--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
-router.get("/", shopController.middlewareSample, shopController.homePage); //middlware runs, then when complete, calls next part of requst
-router.get("/auth", shopController.authMiddleware, shopController.authPage);
+router.get("/", shopController.homePage); //middlware runs, then when complete, calls next part of requst
+
 module.exports = router;

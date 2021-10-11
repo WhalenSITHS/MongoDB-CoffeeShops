1f9177c4ffea00c98348e80f9852ef626cc5f23f
michael whalen <michael.whalen@sitechhs.com>
2021-10-11 12:07:49 -0400

    fake middleware done

--- Routes/index.js
+++ Routes/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
 router.get("/", shopController.middlewareSample, shopController.homePage); //middlware runs, then when complete, calls next part of requst
-
+router.get("/auth", shopController.authMiddleware, shopController.authPage);
 module.exports = router;
--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const routes = require("./Routes/index");
+// Takes the raw requests and turns them into usable properties on req.body
+/* app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); */
+//Body Parser deprecated as of Express 4.16
+app.use(express.json());
+app.use(express.urlencoded());
 //routes, imported from routes folder above
 
 app.use("/", routes);
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });

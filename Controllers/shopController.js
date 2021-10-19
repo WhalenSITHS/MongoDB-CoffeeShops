c26c6046e3e3a6c755c9e23b776fd9725fc9f940
michael whalen <michael.whalen@sitechhs.com>
2021-10-18 20:34:45 -0400

    Done

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,3 +1,4 @@
+const Shop = require("../Models/Stores");
 exports.homePage = (req, res) => {
   const stores = ["Dunkin", "Tim Hortons", "Starbucks"];
   try {
@@ -5,5 +6,15 @@ exports.homePage = (req, res) => {
     res.json([stores, req.name]); //if we want to send multiple "things" back to the user we need to use an array or object. Can't simply use , as that denotes status codes
   } catch (error) {
     console.log(error);
+  }
+};
+
+exports.createShop = async (req, res) => {
+  try {
+    const shop = new Shop(req.body);
+    await shop.save();
+    res.json(shop);
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
--- Routes/index.js
+++ Routes/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
 router.get("/", shopController.homePage); //middlware runs, then when complete, calls next part of requst
-
+router.post("/add", shopController.createShop);
 module.exports = router;

5e036a005653ddcba27381c60ee4461fb7a2e018
mr. whalen <michael.whalen@sitechhs.com>
2021-12-14 07:54:33 -0500

    sdf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 require("./db/mongoose"); //ensures mongoose runs and connects
 const routes = require("./Routes/index");

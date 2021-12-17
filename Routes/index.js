0ab94714509ad06398d21811f41b680a04f04d34
mr. whalen <michael.whalen@sitechhs.com>
2021-12-17 13:34:26 -0500

    dfghdfghdfgh

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = new express.Router();
+const passport = require("passport");
+require("../config/passport-setup");
 const shopController = require("../Controllers/shopController");
 router.get("/", shopController.homePage); //middlware runs, then when complete, calls next part of requst
 router.post("/add", shopController.createShop);
 router.get("/shops", shopController.getShops);
 router.patch("/shops/:id", shopController.updateShop);
 router.delete("/shops/:id", shopController.deleteShop);
+// auth with google+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"],
+  })
+);
+
+// callback route for google to redirect to
+router.get("/auth/google/redirect", (req, res) => {
+  res.send("you reached the redirect URI");
+});
 module.exports = router;
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,16 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-require("dotenv").config({ path: "secrets.env" });
+require("dotenv").config({ path: "secret.env" });
 console.log(process.env.clientID);
 passport.use(
   new GoogleStrategy(
     {
       // options for google strategy
+      callbackURL: "/auth/google/redirect",
       clientID: `${process.env.clientID}`,
       clientSecret: `${process.env.clientSecret}` /* add your client secret here */,
     },
-    () => {
+    (accessToken, refreshToken, profile, done) => {
       // passport callback function
+      console.log("passport callback function fired:");
+      console.log(profile);
     }
   )
 );

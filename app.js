b520a85918c600f3b9114477774ce484cfc8b342
michael whalen <michael.whalen@sitechhs.com>
2021-12-19 22:13:25 -0500

    ready

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
-const { auth } = require("express-oauth2-jwt-bearer");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 const cors = require("cors");
 require("./db/mongoose"); //ensures mongoose runs and connects
-const routes = require("./Routes/index");
+//const routes = require("./Routes/index");
 app.use(cors());
-// Takes the raw requests and turns them into usable properties on req.body
-/* app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); */
-//Body Parser deprecated as of Express 4.16
+
 app.use(express.json());
-app.use(express.urlencoded());
-//routes, imported from routes folder above
+app.use(express.urlencoded({ extended: true }));
+// Create middleware for checking the JWT
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-hr-9upb4.us.auth0.com/.well-known/jwks.json`,
+  }),
 
-const checkJwt = auth({
-  issuerBaseURL: "https://dev-hr-9upb4.us.auth0.com",
-  audience: "http://localhost:3000",
+  // Validate the audience and the issuer
+  audience: "http://localhost:3000", //replace with your API's audience, available at Dashboard > APIs
+  issuer: "https://dev-hr-9upb4.us.auth0.com/",
+  algorithms: ["RS256"],
 });
 
-// Define an endpoint that must be called with an access token
-app.get("/api/external", checkJwt, (req, res) => {
-  res.send({
-    msg: "Your Access Token was successfully validated!",
-  });
-});
+// Enable the use of request body parsing middleware - code omitted
 
+// create timesheets API endpoint - code omitted
+app.get("/authorized", checkJwt, function (req, res) {
+  res.send("Secured Resource");
+});
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
